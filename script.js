33bb63ab0323d5cdeeb63a28ea867bbc89c38f60
Matic <matic687@gmail.com>
2021-03-15 15:24:53 +0100

    Added the work hours feature

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-var now = new Date()
+var now = new Date();
 var konec = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 16, 0, 0);
-var arrival = new Date()
+var arrival = new Date();
+var wHours = 8;
 
 var x = setInterval(function() {
 
@@ -17,38 +18,43 @@ var x = setInterval(function() {
     
     document.getElementById("secondsBarL").style.width = "" + s / 0.6 + "vw";
     document.getElementById("minutesBarL").style.width = "" + m / 0.6 + "vw";
-    document.getElementById("hoursBarL").style.width = "" + h / 0.08 + "vw";
+    document.getElementById("hoursBarL").style.width = "" + h * 100 / wHours + "vw";
 
     document.getElementById("secondsBarP").style.width = "" + s1 / 0.6 + "vw";
     document.getElementById("minutesBarP").style.width = "" + m1 / 0.6 + "vw";
-    document.getElementById("hoursBarP").style.width = "" + h1 / 0.08 + "vw";
+    document.getElementById("hoursBarP").style.width = "" + h1 * 100 / wHours + "vw";
 
     if (h > 0){
-    document.getElementById("timeLeft").innerHTML = h + ":" + addZero(m) + ":" + addZero(s);
+        document.getElementById("timeLeft").innerHTML = h + ":" + addZero(m) + ":" + addZero(s);
     }
-    else{
-    document.getElementById("timeLeft").innerHTML = addZero(m) + ":" + addZero(s);
+    else if(razlikaDoKonca <= 0){
+        document.getElementById("remainingContainer").style.display = "none";
     }
-    if (h1 > 0){
-    document.getElementById("timePassed").innerHTML = h1 + ":" + addZero(m1) + ":" + addZero(s1);
-    }
-    else{
-    document.getElementById("timePassed").innerHTML = addZero(m1) + ":" + addZero(s1);
-    }
+    else
+        document.getElementById("timeLeft").innerHTML = addZero(m) + ":" + addZero(s);
+
+    if (h1 > 0)
+        document.getElementById("timePassed").innerHTML = h1 + ":" + addZero(m1) + ":" + addZero(s1);
+    else
+        document.getElementById("timePassed").innerHTML = addZero(m1) + ":" + addZero(s1);
     
 }, 1000);
 
 var y = setInterval(function() {
     var n = new Date()
     var dnevi = ['Ponedeljek', 'Torek', 'Sreda', 'Četrtek', 'Petek', 'Sobota', 'Nedelja'];
-    document.getElementById("todaysDate").innerHTML = dnevi[n.getDay() - 1] + ", " + n.getDate() + "." + (n.getMonth() + 1) + "." + n.getFullYear() + "<br>" + n.getHours() + "." + n.getMinutes();
+    document.getElementById("todaysDate").innerHTML = dnevi[n.getDay() - 1] + ", " + n.getDate() + "." + (n.getMonth() + 1) + "." + n.getFullYear() + "<br>" + n.getHours() + "." + addZero(n.getMinutes());
 }, 1000);
 
-function getArrival() {
+function setArrival() {
     arrival = document.getElementById("arrival").value
     arrival = new Date(now.getFullYear(), now.getMonth(), now.getDate(), arrival.split(":")[0], arrival.split(":")[1], 0);
-    konec = new Date(now.getFullYear(), now.getMonth(), now.getDate(), arrival.getHours() + 8, arrival.getMinutes(), 0);
-    document.getElementById("arrivalDeparture").innerHTML = "Arrived at " + arrival.getHours() + "." +  arrival.getMinutes() + ", leaving at " + konec.getHours() + "." + konec.getMinutes();
+    konec = new Date(arrival.getFullYear(), arrival.getMonth(), arrival.getDate(), arrival.getHours() + parseInt(wHours), arrival.getMinutes(), 0);
+    document.getElementById("arrivalDeparture").innerHTML = "Arrived at " + arrival.getHours() + "." +  addZero(arrival.getMinutes()) + ", leaving at " + konec.getHours() + "." + addZero(konec.getMinutes());
+    if(new Date(konec - new Date()) > 0)
+        document.getElementById("remainingContainer").style.display = "block";
+    else
+        document.getElementById("remainingContainer").style.display = "none";
 }
 var getJSON = function(url, callback) {
     var xhr = new XMLHttpRequest();
@@ -96,6 +102,11 @@ var getBusTimes = setInterval(function() {
     });
 }, 10000);
 
+function setWorkingHours(){
+    wHours = document.getElementById("workingHours").value;
+    setArrival();
+}
+
 function onLoad(){
     getJSON('https://prominfo.projekti.si/lpp_rc/api/704151', function(err, data) {
     if (err !== null){
@@ -104,16 +115,16 @@ function onLoad(){
     else{
         var text = "In"
         for(var i=0; i<data.arrivals[0].arrivals.length; i++){
-        var minutes = data.arrivals[0].arrivals[i].minutes
-        if (i == data.arrivals[0].arrivals.length - 2){
-            text = text + " " + minutes + " and"
-        }
-        else if (i == data.arrivals[0].arrivals.length - 1){
-            text = text + " " + minutes + " minutes."
-        }
-        else{
-            text = text + " " + minutes + ","
-        }
+            var minutes = data.arrivals[0].arrivals[i].minutes
+            if (i == data.arrivals[0].arrivals.length - 2){
+                text = text + " " + minutes + " and"
+            }
+            else if (i == data.arrivals[0].arrivals.length - 1){
+                text = text + " " + minutes + " minutes."
+            }
+            else{
+                text = text + " " + minutes + ","
+            }
         }
         document.getElementById("bus").innerHTML = text;
     }
